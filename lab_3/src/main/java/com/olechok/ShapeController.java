5e211d1cb4d51537f6c4b80ca2bc09aaf9315265
OlhaMish <olgami1402@gmail.com>
2024-10-03 20:15:19 +0300

    feat: improve ShapeController functional

--- lab_3/src/main/java/com/olechok/ShapeController.java
+++ lab_3/src/main/java/com/olechok/ShapeController.java
@@ -1,16 +1,46 @@
 package com.olechok;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class ShapeController {
-    private Shape model;
+class ShapeController {
+    private Shape[] shapes;
     private ShapeView view;
 
-    public ShapeController(Shape shape, ShapeView view) {
-        this.model = shape;
+    public ShapeController(Shape[] shapes, ShapeView view) {
+        this.shapes = shapes;
         this.view = view;
     }
 
-    public void updateView() {
-        view.displayShapeType(model.getClass().getSimpleName());
-        view.displayArea(model.calcArea());
+    public void displayShapes() {
+        view.displayShapeList(shapes);
+    }
+
+    public void calculateTotalArea() {
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.calcArea();
+        }
+        view.displayTotalArea(totalArea);
+    }
+
+    public void calculateTotalAreaByType(Class<? extends Shape> shapeType) {
+        double totalArea = 0;
+        String typeName = shapeType.getSimpleName();
+
+        for (Shape shape : shapes) {
+            if (shapeType.isInstance(shape)) {
+                totalArea += shape.calcArea();
+            }
+        }
+        view.displayTotalAreaByType(totalArea, typeName);
+    }
+
+    public void sortShapesByArea() {
+        Arrays.sort(shapes, Comparator.comparingDouble(Shape::calcArea));
+    }
+
+    public void sortShapesByColor() {
+        Arrays.sort(shapes, Comparator.comparing(Shape::calcArea));
     }
 }
+
