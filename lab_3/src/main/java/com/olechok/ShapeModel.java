e46b94541dd349f22c054e000679dad4ff840f87
OlhaMish <olgami1402@gmail.com>
2024-10-04 10:58:03 +0300

    feat: add ShapeModel that work with shapes

---
+++ lab_3/src/main/java/com/olechok/ShapeModel.java
@@ -0,0 +1,53 @@
+package com.olechok;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ShapeModel {
+    private Shape[] shapes;
+
+    public ShapeModel(Shape[] shapes) {
+        this.shapes = shapes;
+    }
+
+    public double calculateTotalArea() {
+        if (shapes == null || shapes.length == 0) {
+            return 0.0;
+        }
+
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.calcArea();
+        }
+        return totalArea;
+    }
+
+    public double calculateTotalAreaByType(Class<? extends Shape> shapeType) {
+        if (shapes == null || shapes.length == 0) {
+            return 0.0;
+        }
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            if (shapeType.isInstance(shape)) {
+                totalArea += shape.calcArea();
+            }
+        }
+        return totalArea;
+    }
+
+    public void sortShapesByArea() {
+        Arrays.sort(shapes, Comparator.comparingDouble(Shape::calcArea));
+    }
+
+    public void sortShapesByColor() {
+        Arrays.sort(shapes, Comparator.comparing(shape -> shape.shapeColor));
+    }
+
+    public Shape[] getShapes() {
+        return shapes;
+    }
+
+    protected void setShapes(int numberOfShapes) {
+        shapes = ShapeGenerator.generateRandomShapes(numberOfShapes);
+    }
+}
