7efe2200bb21ec4e3126b723071f01235e1fca7c
OlhaMish <olgami1402@gmail.com>
2024-10-04 10:54:41 +0300

    feat: remove Main and add ShapeApp

---
+++ lab_3/src/main/java/com/olechok/ShapeApp.java
@@ -0,0 +1,48 @@
+package com.olechok;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        ShapeModel model = new ShapeModel(null);
+        ShapeView view = new ShapeView();
+        ShapeController controller = new ShapeController(model, view);
+
+        boolean running = true;
+        InputUtils inputUtils = new InputUtils();
+
+        while (running) {
+            inputUtils.printMenu();
+
+            int choice = inputUtils.getChoice();
+            switch (choice) {
+                case 1:
+                    controller.setShapes(10);
+                    System.out.println("Generated new shapes:");
+                    controller.displayShapes();
+                    break;
+                case 2:
+                    controller.calculateTotalArea();
+                    break;
+                case 3:
+                    String shapeType = inputUtils.getShapeType();
+                    controller.getTotalAreaByType(shapeType);
+                    break;
+                case 4:
+                    System.out.println("\nShapes sorted by Area:");
+                    controller.sortShapesByArea();
+                    break;
+                case 5:
+                    System.out.println("\nShapes sorted by Color:");
+                    controller.sortShapesByColor();
+                    break;
+                case 6:
+                    running = false;
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+        inputUtils.scannerClose();
+    }
+}
+
