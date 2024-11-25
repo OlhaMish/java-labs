0636f0fd5b983433c6675d25a86e854d110c4d3d
OlhaMish <olgami1402@gmail.com>
2024-11-25 16:02:17 +0200

    feat: add Translator class to manage dictionary and phrase translation

---
+++ lab_6/src/main/java/com/olechok/Translator.java
@@ -0,0 +1,30 @@
+package com.olechok;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translator {
+    private final Map<String, String> dictionary;
+
+    public Translator() {
+        this.dictionary = new HashMap<>();
+    }
+
+    public void addWord(String englishWord, String ukrainianWord) {
+        dictionary.put(englishWord.toLowerCase(), ukrainianWord.toLowerCase());
+    }
+
+    public String translatePhrase(String phrase) {
+        String[] words = phrase.toLowerCase().split("\\s+");
+        StringBuilder translatedPhrase = new StringBuilder();
+
+        for (String word : words) {
+            String translatedWord = dictionary.getOrDefault(word, "[" + word + "]");
+            translatedPhrase.append(translatedWord).append(" ");
+        }
+
+        return translatedPhrase.toString().trim();
+    }
+
+
+}
