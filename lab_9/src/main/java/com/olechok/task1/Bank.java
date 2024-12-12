0d0816c1d80bce3ebfd80438169c7e09fb506664
OlhaMish <olgami1402@gmail.com>
2024-12-12 20:32:20 +0200

    feat: remove comments

--- lab_9/src/main/java/com/olechok/task1/Bank.java
+++ lab_9/src/main/java/com/olechok/task1/Bank.java
@@ -3,7 +3,6 @@ package com.olechok.task1;
 
 public class Bank {
     public void transfer(Account from, Account to, int amount) {
-        // Порядок блокування залежить від хеш-кодів об'єктів
         Account first = from.hashCode() < to.hashCode() ? from : to;
         Account second = from.hashCode() < to.hashCode() ? to : from;
 
